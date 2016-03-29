70c350234bf244c14869d2fd7f699138cbe45042
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-03-29 08:22:54 +0800

    initial project.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ mocha.go
@@ -0,0 +1,75 @@
+package tests
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+)
+
+// request handling func type to replace gin.HandlerFunc
+type RequestFunc func(*gin.Context)
+
+// response handling func type
+type ResponseFunc func(*httptest.ResponseRecorder)
+
+type RequestConfig struct {
+	Method      string
+	Path        string
+	Body        string
+	Headers     map[string]string
+	Middlewares []gin.HandlerFunc
+	Handler     RequestFunc
+	Finaliser   ResponseFunc
+}
+
+func RunRequest(rc RequestConfig) {
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+
+	if rc.Middlewares != nil && len(rc.Middlewares) > 0 {
+		for _, mw := range rc.Middlewares {
+			r.Use(mw)
+		}
+	}
+
+	qs := ""
+	if strings.Contains(rc.Path, "?") {
+		ss := strings.Split(rc.Path, "?")
+		rc.Path = ss[0]
+		qs = ss[1]
+	}
+
+	body := bytes.NewBufferString(rc.Body)
+
+	req, _ := http.NewRequest(rc.Method, rc.Path, body)
+
+	if len(qs) > 0 {
+		req.URL.RawQuery = qs
+	}
+
+	if len(rc.Headers) > 0 {
+		for k, v := range rc.Headers {
+			req.Header.Set(k, v)
+		}
+	} else if rc.Method == "POST" || rc.Method == "PUT" {
+		if strings.HasPrefix(rc.Body, "{") {
+			req.Header.Set("Content-Type", "application/json")
+		} else {
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		}
+	}
+
+	r.Handle(rc.Method, rc.Path, func(c *gin.Context) {
+		//change argument if necessary here
+		rc.Handler(c)
+	})
+
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	if rc.Finaliser != nil {
+		rc.Finaliser(w)
+	}
+}
