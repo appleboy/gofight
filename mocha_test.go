646de27baf849322fb45df3abdde9e6d4c8a4308
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-03-29 10:32:09 +0800

    test query parameter.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- mocha_test.go
+++ mocha_test.go
@@ -19,6 +19,16 @@ func textHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello World")
 }
 
+func queryHandler(c *gin.Context) {
+	text := c.Query("text")
+	foo := c.Query("foo")
+
+	c.JSON(http.StatusOK, gin.H{
+		"hello": text,
+		"foo": foo,
+	})
+}
+
 func TestHelloWorld(t *testing.T) {
 	r := &RequestConfig{
 		Handler: helloHandler,
@@ -44,6 +54,25 @@ func TestHeader(t *testing.T) {
 		Callback: func(r *httptest.ResponseRecorder) {
 
 			assert.Equal(t, r.Body.String(), "Hello World")
+			assert.Equal(t, r.Code, http.StatusOK)
+		},
+	}
+
+	r.Run()
+}
+
+func TestQuery(t *testing.T) {
+	r := &RequestConfig{
+		Path: "/hello?text=world&foo=bar",
+		Handler: queryHandler,
+		Callback: func(r *httptest.ResponseRecorder) {
+			data := []byte(r.Body.String())
+
+			hello, _ := jsonparser.GetString(data, "hello")
+			foo, _ := jsonparser.GetString(data, "foo")
+
+			assert.Equal(t, hello, "world")
+			assert.Equal(t, foo, "bar")
 			assert.Equal(t, r.Code, http.StatusOK)
 		},
 	}
