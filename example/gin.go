75baa5975d3cbc68045c40d5a7f94d69c5c226a6
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-09-28 22:09:20 +0800

    Add example for #54
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example/gin.go
+++ example/gin.go
@@ -1,6 +1,7 @@
 package example
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,12 +11,18 @@ func ginHelloHandler(c *gin.Context) {
 	c.String(http.StatusOK, "Hello World")
 }
 
+func ginUserHandler(c *gin.Context) {
+	name := c.Param("name")
+	c.String(http.StatusOK, fmt.Sprintf("Hello, %s", name))
+}
+
 // GinEngine is gin router.
 func GinEngine() *gin.Engine {
 	gin.SetMode(gin.TestMode)
 	r := gin.New()
 
 	r.GET("/", ginHelloHandler)
+	r.GET("/user/:name", ginUserHandler)
 
 	return r
 }
--- example/gin_test.go
+++ example/gin_test.go
@@ -17,3 +17,14 @@ func TestGinHelloWorld(t *testing.T) {
 			assert.Equal(t, http.StatusOK, r.Code)
 		})
 }
+
+func TestGinHelloHandler(t *testing.T) {
+	r := gofight.New()
+
+	r.GET("/user/appleboy").
+		SetDebug(true).
+		Run(GinEngine(), func(r gofight.HTTPResponse, rq gofight.HTTPRequest) {
+			assert.Equal(t, "Hello, appleboy", r.Body.String())
+			assert.Equal(t, http.StatusOK, r.Code)
+		})
+}
