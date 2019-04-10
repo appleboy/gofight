7a1715544f690610c704138c86dabfb1e7cbe053
Bo-Yi Wu <appleboy.tw@gmail.com>
2019-04-10 10:33:18 +0800

    fix lint
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- example/beego.go
+++ example/beego.go
@@ -9,6 +9,7 @@ type UserController struct {
 	beego.Controller
 }
 
+// SayHelloWorld for say hello
 func (c *UserController) SayHelloWorld() {
 	c.Ctx.ResponseWriter.Status = 200
 	c.Ctx.ResponseWriter.Write([]byte("Hello, World"))
